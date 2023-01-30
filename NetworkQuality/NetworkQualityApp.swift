f3ec1992c9ddc5004f20af971eb937c1c1fbb165
Gergely Kiss <gergelytamas.kiss@gmail.com>
2023-01-30 21:50:44 +0100

    Initial Commit

---
+++ NetworkQuality/NetworkQualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkQualityApp.swift
+//  NetworkQuality
+//
+//  Created by Gergely Kiss on 2023. 01. 30..
+//
+
+import SwiftUI
+
+@main
+struct NetworkQualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
